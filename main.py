608393c6a3fcc974ac7cdca405e7e9ba61ec0195
oriBetelgeuse <arkhipkin.v98@gmail.com>
2022-02-03 01:12:28 +0300

    add requirements

---
+++ main.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+
+from omegaconf import OmegaConf
+
+from fb_baseline.train_detection_vqa import run_train
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--config', type=str, help='config path')
+    args = parser.parse_args()
+    conf = OmegaConf(args.config)
+    run_train(conf)
